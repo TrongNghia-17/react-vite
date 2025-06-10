7e26337a5ea2ed25d9143ddbe2fc02b32ae2d7b3
NghiaHT <huynhtrongnghia15n@gmail.com>
2025-06-10 22:45:37 +0700

    video 34

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,24 @@ import TodoNew from './components/todo/TodoNew';
 import reactLogo from './assets/react.svg';
 
 const App = () => {
+  const hoidanit = "Eric";
+  const age = 25;
+  const data = {
+    address: "hcm",
+    country: "vietnam"
+  }
+
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew />
-      <TodoData />
+      <TodoData
+        name={hoidanit}
+        age={age}
+        data={data}
+      />
       <div className='todo-image'>
-        <img src={reactLogo} className='logo'/>
+        <img src={reactLogo} className='logo' />
       </div>
     </div>
   )
