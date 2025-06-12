218dcae25bb5ac1268363c22a8731ab748ecaf56
NghiaHT <huynhtrongnghia15n@gmail.com>
2025-06-13 00:01:29 +0700

    video 52

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -5,8 +5,8 @@ const Header = () => {
     return (
         <ul>
             <li><NavLink class="active" to="/">Home</NavLink></li>
-            <li><NavLink to="users">Users</NavLink></li>
-            <li><NavLink to="products">Products</NavLink></li>
+            <li><NavLink to="/users">Users</NavLink></li>
+            <li><NavLink to="/books">Books</NavLink></li>
         </ul>
 
     )
