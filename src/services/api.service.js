5bf0adac1b41c0b2a85aa445a4d2a64ab4e279f4
NghiaHT <huynhtrongnghia15n@gmail.com>
2025-06-26 22:12:18 +0700

    video 73

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -2,7 +2,7 @@
 import axios from "./axios.customize";
 
 const createUserAPI = (fullName, email, password, phone) => {
-    const URL_BACKEND = "/api/v1/user"
+    const URL_BACKEND = "/api/v1/user";
     const data = {
         fullName: fullName,
         email: email,
@@ -16,6 +16,11 @@ const updateUserAPI = () => {
 
 }
 
+const fetchAllUserAPI = () => {
+    const URL_BACKEND = "/api/v1/user";
+    return axios.get(URL_BACKEND);
+}
+
 export {
-    createUserAPI, updateUserAPI
+    createUserAPI, updateUserAPI, fetchAllUserAPI
 }
