09b00a8304c329de44baf0485d5d11cb35fd54dc
NghiaHT <huynhtrongnghia15n@gmail.com>
2025-06-28 22:28:33 +0700

    video 86

--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -7,7 +7,10 @@ import { Popconfirm, notification } from 'antd';
 import { deleteUserAPI } from '../../services/api.service';
 
 const UserTable = (props) => {
-    const { dataUsers, loadUser } = props;
+    const { dataUsers, loadUser,
+        current, pageSize, total,
+        setCurrent, setPageSize
+    } = props;
 
     const [isModalUpdateOpen, setIsModalUpdateOpen] = useState(false);
 
@@ -93,12 +96,25 @@ const UserTable = (props) => {
         },
     ];
 
+    const onChange = (pagination, filters, sorter, extra) => {
+
+    };
+
     return (
         <>
             <Table
                 columns={columns}
                 dataSource={dataUsers}
                 rowKey={"_id"}
+                pagination={
+                    {
+                        current: current,
+                        pageSize: pageSize,
+                        showSizeChanger: true,
+                        total: total,
+                        showTotal: (total, range) => { return (<div> {range[0]}-{range[1]} trên {total} rows</div>) }
+                    }}
+                onChange={onChange}
             />
             <UpdateUserModal
                 isModalUpdateOpen={isModalUpdateOpen}
--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -5,14 +5,22 @@ import { useEffect, useState } from 'react';
 
 const UserPage = () => {
     const [dataUsers, setDataUsers] = useState([]);
+    const [current, setCurrent] = useState(1);
+    const [pageSize, setPageSize] = useState(5);
+    const [total, setTotal] = useState(0);
 
     useEffect(() => {
         loadUser();
     }, []);
 
     const loadUser = async () => {
-        const res = await fetchAllUserAPI();
-        setDataUsers(res.data)
+        const res = await fetchAllUserAPI(current, pageSize);
+        if (res.data) {
+            setDataUsers(res.data.result);
+            setCurrent(res.data.meta.current);
+            setPageSize(res.data.meta.pageSize);
+            setTotal(res.data.meta.total);
+        }
     }
 
     return (
@@ -21,6 +29,11 @@ const UserPage = () => {
             <UserTable
                 dataUsers={dataUsers}
                 loadUser={loadUser}
+                current={current}
+                pageSize={pageSize}
+                total={total}
+                setCurrent={setCurrent}
+                setDataUsers={setPageSize}
             />
         </div>
     )
